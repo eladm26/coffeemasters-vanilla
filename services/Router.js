f3e777639bd54ecda83ccf870b82e31c73ca3ccb
Elad Moshe <eladm26@gmail.com>
2024-05-15 09:45:29 +0300

    qa

--- services/Router.js
+++ services/Router.js
@@ -1,17 +1,56 @@
 const Router = {
     init: function init() {
-        document.querySelectorAll("a.navlink").forEach(a => {
-            a.addEventListener("click", event => {
+        document.querySelectorAll("a.navlink").forEach((a) => {
+            a.addEventListener("click", (event) => {
                 event.preventDefault();
                 const url = event.target.getAttribute("href");
-                console.log("this", this);
                 this.go(url);
-            })
+            });
         });
+        window.addEventListener("popstate", (event) => {
+            this.go(event.state.route, false);
+        });
+        this.go(location.pathname);
     },
-    go: function(route, addToHistory=true) {
+    go: function (route, addToHistory = true) {
         console.log(`Going to ${route}`);
-    }
-}
+
+        if (addToHistory) {
+            history.pushState({ route }, "", route);
+        }
+
+        var pageElement = null;
+
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your Order";
+                break;
+            default:
+                if (route.startWith("/product-")) {
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    const paramId = route.substring(route.lastIndexOf("-") + 1);
+                    pageElement.dataset.id = paramId;
+                }
+                break;
+        }
+
+        if (pageElement) {
+            const cache = document.querySelector("main");
+            if (cache.children.length > 0) {
+                cache.children[0].remove();
+            }
+            document.querySelector("main").appendChild(pageElement);
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+    },
+};
 
 export default Router;
