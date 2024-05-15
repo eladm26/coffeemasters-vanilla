c7efd5552927c77d37760a89732527d84bd34906
Elad Moshe <eladm26@gmail.com>
2024-05-15 11:09:48 +0300

    web component

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import Store from "./services/Store.js";
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
+// Link my web component
+import { MenuPage } from "./components/MenuPage.js";
+import { OrdersPage } from "./components/OrderPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
 
 window.app = {};
 app.store = Store;
@@ -11,10 +15,22 @@ window.addEventListener("DOMContentLoaded", async () => {
     app.router.init();
 });
 
-const $ = function(args){ return document.querySelector(args);}
-const $$ = function(args){ return document.querySelectorAll(args);}
+const $ = function (args) {
+    return document.querySelector(args);
+};
+const $$ = function (args) {
+    return document.querySelectorAll(args);
+};
 
-HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
-HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
-HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
-HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
+HTMLElement.prototype.on = function (a, b, c) {
+    return this.addEventListener(a, b, c);
+};
+HTMLElement.prototype.off = function (a, b) {
+    return this.removeEventListener(a, b);
+};
+HTMLElement.prototype.$ = function (s) {
+    return this.querySelector(s);
+};
+HTMLElement.prototype.$$ = function (s) {
+    return this.querySelectorAll(s);
+};
